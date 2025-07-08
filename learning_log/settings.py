b714ee3ae30a0453c011a2707b037f421e69f50f
devXpress <ekpenyonabasi@gmail.com>
2025-07-08 23:41:21 +0100

    Fix DATABASES config for Heroku with dj-database-url

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -78,10 +78,11 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local dev
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
